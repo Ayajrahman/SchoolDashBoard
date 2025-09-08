093a7343793ee6ea2dca9ea19be80083650c9c1c
Ayajrahman <ayajrahman2004@gmail.com>
2025-09-08 12:14:39 +0530

    add Dashboard name mistake

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // src/App.js
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Dashboard from "./pages/Dashboard";
+import Dashboard from "./pages/DashBoard.jsx";
 import Students from "./pages/Students";
 // import Reports from "./pages/Reports"; // example extra page
 
